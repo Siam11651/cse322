855ce077b281567719075ab78a3c1839edaa7430
Siam11651 <siam11651@outlook.com>
2023-06-26 19:19:09 +0600

    kaaj kori

--- offline-1/server/src/Offline_1/Client.java
+++ offline-1/server/src/Offline_1/Client.java
@@ -1,6 +1,5 @@
 package Offline_1;
 
-import java.io.EOFException;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -8,8 +7,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.SocketException;
-import java.net.SocketTimeoutException;
 import java.util.Collections;
 import java.util.Hashtable;
 import java.util.Random;
