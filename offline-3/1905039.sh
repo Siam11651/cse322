ef9b25baa3e4968a5467e611af8e1623f9e54955
Siam11651 <siam11651@outlook.com>
2023-08-29 02:25:16 +0600

    kaj kori

--- offline-3/1905039.sh
+++ offline-3/1905039.sh
@@ -3,39 +3,43 @@ outdir="out"
 rm -rf "$outdir"
 mkdir "$outdir"
 
-for algorithm in $(echo "TcpNewReno TcpHighSpeed TcpAdaptiveReno")
+for algorithm in $(echo "TcpWestwoodPlus TcpHighSpeed TcpAdaptiveReno")
 do
-    throughput_vs_data_rate_file="throughput-vs-data-rate-${algorithm,,}.dat"
-    throughput_vs_loss_rate_file="throughput-vs-loss-rate-${algorithm,,}.dat"
-    data_rate_plot_file="throughput-vs-data-rate-${algorithm,,}.png"
-    loss_rate_plot_file="throughput-vs-loss-rate-${algorithm,,}.png"
+    data_rate_file="data-rate-${algorithm,,}.dat"
+    loss_rate_file="loss-rate-${algorithm,,}.dat"
+    data_rate_throughput_plot_file="throughput-vs-data-rate-${algorithm,,}.png"
+    loss_rate_throughput_plot_file="throughput-vs-loss-rate-${algorithm,,}.png"
+    data_rate_fairness_plot_file="fairness-vs-data-rate-${algorithm,,}.png"
+    loss_rate_fairness_plot_file="fairness-vs-loss-rate-${algorithm,,}.png"
     cwnd_data1_file="congestion-window-1-${algorithm,,}.dat"
     cwnd_data2_file="congestion-window-2-${algorithm,,}.dat"
     cwnd_plot_file="cwnd-vs-time-${algorithm,,}.png"
 
     touch "$outdir/$throughput_vs_data_rate_file"
-    echo "# DataRate Throughput1 Throughput2" >> "$outdir/$throughput_vs_data_rate_file"
+    echo "# DataRate Throughput1 Throughput2 Fairness" >> "$outdir/$data_rate_file"
 
-    for i in $(seq 10 10 300)
+    for i in $(seq 50 50 300)
     do
-        average_throughput=$(./ns3 run --quiet "scratch/1905039/1905039.cc --algorithm="$algorithm" --bottleneck-rate=$i")
-        echo "$i $average_throughput" >> "$outdir/$throughput_vs_data_rate_file"
+        data=$(./ns3 run --quiet "scratch/1905039/1905039.cc --algorithm="$algorithm" --bottleneck-rate=$i")
+        echo "$i $data" >> "$outdir/$data_rate_file"
     done
 
-    gnuplot -c "scratch/throughput.plt" "$algorithm" "TcpWestwoodPlus" "$outdir/$throughput_vs_data_rate_file" "$outdir/$data_rate_plot_file"
+    gnuplot -c "scratch/throughput.plt" "TcpNewReno" "$algorithm" "$outdir/$data_rate_file" "$outdir/$data_rate_throughput_plot_file" "Data Rate (Mbps)"
+    gnuplot -c "scratch/fairness.plt" "$outdir/$data_rate_file" "$outdir/$data_rate_fairness_plot_file" "Data Rate (Mbps)"
 
-    touch "$outdir/$throughput_vs_loss_rate_file"
-    echo "# LossRate Throughput1 Throughput2" >> "$outdir/$throughput_vs_loss_rate_file"
+    touch "$outdir/$loss_rate_file"
+    echo "# LossRate Throughput1 Throughput2 Fairness" >> "$outdir/$loss_rate_file"
 
-    for i in $(seq -6 0.1 -2)
+    for i in $(seq -6 0.5 -2)
     do
-        average_throughput=$(./ns3 run --quiet "scratch/1905039/1905039.cc --algorithm="$algorithm" --loss-rate-exponent=$i")
-        echo "$i $average_throughput" >> "$outdir/$throughput_vs_loss_rate_file"
+        data=$(./ns3 run --quiet "scratch/1905039/1905039.cc --algorithm="$algorithm" --loss-rate-exponent=$i")
+        echo "$i $data" >> "$outdir/$loss_rate_file"
     done
 
-    gnuplot -c "scratch/throughput.plt" "$algorithm" "TcpWestwoodPlus" "$outdir/$throughput_vs_loss_rate_file" "$outdir/$loss_rate_plot_file"
+    gnuplot -c "scratch/throughput.plt" "TcpNewReno" "$algorithm" "$outdir/$loss_rate_file" "$outdir/$loss_rate_throughput_plot_file" "Loss Rate Exponent"
+    gnuplot -c "scratch/fairness.plt" "$outdir/$loss_rate_file" "$outdir/$loss_rate_fairness_plot_file" "Loss Rate Exponent"
 
     ./ns3 run --quiet "scratch/1905039/1905039.cc --algorithm="$algorithm" --trace-congestion" > /dev/null
 
-    gnuplot -c "scratch/cwnd-vs-time.plt" "$algorithm" "TcpWestwoodPlus" "$outdir/$cwnd_data1_file" "$outdir/$cwnd_data2_file" "$outdir/$cwnd_plot_file"
+    gnuplot -c "scratch/cwnd-vs-time.plt" "TcpNewReno" "$algorithm" "$outdir/$cwnd_data1_file" "$outdir/$cwnd_data2_file" "$outdir/$cwnd_plot_file"
 done
--- offline-3/fairness.plt
+++ offline-3/fairness.plt
@@ -1,3 +1,5 @@
 set terminal png size 640, 480
 set output ARG2
+set xlabel ARG3
+set ylabel "Fairness"
 plot ARG1 using 1:4 title "Fairness" with lines
